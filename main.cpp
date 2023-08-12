98c2c0e9f05715bd449ded245edb80e6461ce97e
schmanu <asmanu21@gmail.com>
2023-08-12 19:22:22 -0400

    .

--- main.cpp
+++ main.cpp
@@ -2,16 +2,18 @@
 #include <graphics.h>
 #include <unistd.h>
 
+const int pixelSize = 40;
+
 //Created with/for SDL_bgi
 
-int pixel(int x, int y, int pixelSize) {
-    setfillstyle(SOLID_FILL, WHITE);
+int pixel(int x, int y) {
+    setfillstyle(SOLID_FILL, RED);
     //rectangle(x*pixelSize, ((getmaxy() - y)*pixelSize), (x*pixelSize) + pixelSize, (((getmaxy()) - y)*pixelSize) - pixelSize);
     floodfill(x*pixelSize+1, (getmaxy() - y*pixelSize) - 1, WHITE);
     return 0;
 }
 
-int brese(int x0, int y0, int x1, int y1, int pixelSize, bool midpoint = false) {
+int brese(int x0, int y0, int x1, int y1, bool midpoint = false) {
     int dy = abs(y1 - y0);
     int dx = abs(x1 - x0);
     const int cx = x0 < x1 ? 1 : -1;
@@ -43,8 +45,8 @@ int brese(int x0, int y0, int x1, int y1, int pixelSize, bool midpoint = false) 
         error = ey - ex;
     };
     for(int i = 0; i < delay; i++) {
-        pixel(x0, y0, pixelSize);
-        pixel(x1, y1, pixelSize);
+        pixel(x0, y0);
+        pixel(x1, y1);
         if (error >= 0) {
             error += (2 * dy - 2 * dx);
             x0 += cx;
@@ -64,7 +66,6 @@ int brese(int x0, int y0, int x1, int y1, int pixelSize, bool midpoint = false) 
 int main() {
     //initiate window to correct size
     int size = 32;
-    int pixelSize = 40;
     int resolution = pixelSize * size;
     initwindow(resolution, resolution);
     //grid creation
@@ -72,16 +73,16 @@ int main() {
         rectangle(i, 0, i, resolution);
         rectangle(0, i, resolution, i);
     };
-    brese(0, 0, 31, 5, pixelSize, false);
+    brese(0, 0, 31, 5, false);
     refresh();
-    std::cout << getch();
+    getch();
     return 0;
 };
 
 int errorCalc(int dx, int dy, int mode) {
     return 0;
 }
-int quadLine(int x0, int y0, int x1, int y1, int pixelSize) {
+int quadLine(int x0, int y0, int x1, int y1) {
     //int dy = abs(y1 - y0);
     //int dx = abs(x1 - x0);
     //brese(x0, y0, x1, y1, pixelSize, true);
